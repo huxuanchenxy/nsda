823f3be808d637df4e03127a97db6a8038458776
guanlei <guanlei.sh@enreachedu.com>
2018-01-19 17:57:48 +0800

    nsda

---
+++ nsda.Model/model/t_eventresult.cs
@@ -0,0 +1,52 @@
+//-----------------------------------------------------------------------
+//<copyright>
+// * Copyright (C) 2018 NERICHIST All Rights Reserved
+// * version : 4.0.30319.42000
+// * author  : generated t4 by guanlei
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using Dapper;
+using nsda.Model.enums;
+namespace nsda.Models
+{
+    /// <summary>
+    /// TableModel
+    /// </summary>    
+	[Table("t_eventresult")]
+    public class t_eventresult
+    {
+	   //默认构造函数
+	   public t_eventresult()
+	    {
+			//初始化默认字段
+            createtime = DateTime.Now;
+            updatetime = DateTime.Now;
+            isdelete = false;
+        }
+       
+        [Key]
+	    public int id { get; set; }
+    
+        public int eventId { get; set; }
+    
+        public int eventGroupId { get; set; }
+    
+        public string groupNum { get; set; }
+    
+        public int ranking { get; set; }
+    
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public DateTime updatetime { get; set; }
+    
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime createtime { get; set; }
+    
+        public bool isdelete { get; set; }
+    }
+}
