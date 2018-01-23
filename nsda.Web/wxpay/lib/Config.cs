895b56bfa6677b7604bf410381cef1bce421cf8e
guanlei <guanlei.sh@enreachedu.com>
2018-01-23 11:54:06 +0800

    nsda wxpay

--- nsda.Web/wxpay/lib/Config.cs
+++ nsda.Web/wxpay/lib/Config.cs
@@ -24,7 +24,7 @@ namespace nsda.Web.wxpay
         //=======【证书路径设置】===================================== 
         /* 证书路径,注意应该填写绝对路径（仅退款、撤销订单时需要）
         */
-        public const string SSLCERT_PATH = "/WxPayAPI/cert/apiclient_cert.p12";
+        public const string SSLCERT_PATH = "/wxpay/cert/apiclient_cert.p12";
         public const string SSLCERT_PASSWORD = "1316677801";
 
 
@@ -32,18 +32,18 @@ namespace nsda.Web.wxpay
         //=======【支付结果通知url】===================================== 
         /* 支付结果通知回调url，用于商户接收支付结果
         */
-        public const string NOTIFY_URL = "http://paysdk.weixin.qq.com/example/ResultNotifyPage.aspx";
+        public const string NOTIFY_URL = "http://test.dipot.com/callbcak/resultnotify";
 
         //=======【商户系统后台机器IP】===================================== 
         /* 此参数可手动配置也可在程序中自动获取
         */
-        public const string IP = "8.8.8.8";
+        public const string IP = "0.0.0.0";
 
 
         //=======【代理服务器设置】===================================
         /* 默认IP和端口号分别为0.0.0.0和0，此时不开启代理（如有需要才设置）
         */
-        public const string PROXY_URL = "http://10.152.18.220:8080";
+        public const string PROXY_URL = "0";
 
         //=======【上报信息配置】===================================
         /* 测速上报等级，0.关闭上报; 1.仅错误时上报; 2.全量上报
