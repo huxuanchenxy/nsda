5c973d8ec8666510245047817582ee847c1dd03d
eigerkl@gmail.com <kuaile2>
2018-01-29 17:05:39 +0800

    新增 教练信息

---
+++ html/nsda/nsda/Content/asset/js/load.js
@@ -0,0 +1,4 @@
+﻿layer.msg('加载中', {
+    icon: 16,
+    shade: 0.01
+});
---
+++ html/nsda/nsda/Content/asset/js/selected.js
@@ -0,0 +1,13 @@
+﻿
+$(".address-lists li").click(function () {
+    console.log($(this))
+    $(this).addClass("selected").siblings().removeClass("selected")
+})
+$(".level-lists li").click(function () {
+    console.log($(this))
+    $(this).addClass("selected").siblings().removeClass("selected")
+})
+$(".time-mon li").click(function () {
+    console.log($(this))
+    $(this).addClass("selected").siblings().removeClass("selected")
+})
