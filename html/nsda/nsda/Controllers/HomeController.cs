2faf0351d4c8a27346f6645741c9b4d5917b3ad8
eigerkl@gmail.com <kuaile2>
2018-01-31 17:17:13 +0800

    event

--- html/nsda/nsda/Controllers/HomeController.cs
+++ html/nsda/nsda/Controllers/HomeController.cs
@@ -8,96 +8,115 @@ namespace WebApplication2.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult about()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
-        public ActionResult Aboutp()
+        public ActionResult aboutp()
         {
             ViewBag.Message = "合作伙伴";
             return View();
         }
-        public ActionResult Aboutus()
+        public ActionResult aboutus()
         {
             ViewBag.Message = "联系我们";
             return View();
         }
 
-        public ActionResult Registered()
+        public ActionResult registered()
         {
             ViewBag.Message = "会员登录";
             return View();
         }
-        public ActionResult Regform()
+        public ActionResult regform()
         {
             ViewBag.Message = "会员注册";
             return View();
         }
 
-        public ActionResult PlayerMembercenter()
+        public ActionResult playermembercenter()
         {
             ViewBag.Message = "会员中心";
             return View();
         }
 
-        public ActionResult PlayerCompeSign()
+        public ActionResult playercompesign()
         {
             ViewBag.Message = "比赛报名";
             return View();
         }
-
-        public ActionResult PlayerHadCompe()
+        public ActionResult coachboundplayer()
+        {
+            ViewBag.Message = "比赛报名";
+            return View();
+        }
+        public ActionResult playerhadcompe()
         {
             ViewBag.Message = "已参加比赛";
             return View();
         }
-        public ActionResult PlayerExitCompe()
+        public ActionResult playerexitcompe()
         {
             ViewBag.Message = "退赛信息";
             return View();
         }
-        public ActionResult PlayerScore()
+        public ActionResult playerscore()
         {
             ViewBag.Message = "会员积分查询";
             return View();
         }
-        public ActionResult PlayerInformation()
+        public ActionResult playerinformation()
         {
             ViewBag.Message = "修改个人资料";
             return View();
         }
-        public ActionResult PlayerBoundCoach()
+        public ActionResult playerboundcoach()
         {
             ViewBag.Message = "绑定教练";
             return View();
         }
-        public ActionResult PlayerSource()
+        public ActionResult playersource()
         {
             ViewBag.Message = "会员资源";
             return View();
         }
-        public ActionResult PlayerMessageBox()
+        public ActionResult playermessageBox()
         {
             ViewBag.Message = "消息盒子";
             return View();
         }
-        public ActionResult CoachInformation()
+        public ActionResult coachinformation()
         {
             ViewBag.Message = "Coach Personal Information";
+            return View();
+        }
+        public ActionResult eventmanage()
+        {
+            ViewBag.Message = "比赛进程管理";
+            return View();
+        }
+        public ActionResult eventstart()
+        {
+            ViewBag.Message = "发起赛事";
+            return View();
+        }
+        public ActionResult eventstartlists()
+        {
+            ViewBag.Message = "发起赛事";
             return View();
         }
     }
