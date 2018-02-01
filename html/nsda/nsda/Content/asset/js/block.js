dca2518d7d0ba7dd82e87d954b93aafb57b89a4b
eigerkl@gmail.com <kuaile2>
2018-02-01 10:18:39 +0800

    修改 navpage

---
+++ html/nsda/nsda/Content/asset/js/block.js
@@ -0,0 +1,12 @@
+﻿function $$(id) {
+    return document.getElementById(id);
+}
+window.onload = function () {
+    $$("search").onclick = function () {
+        $$("sel").style.display = "block"
+    };
+    $$("sel").onclick = function () {
+        this.style.display = "none"
+        $$("txt").value = this.options[this.selectedIndex].text;
+    };
+};
--- html/nsda/nsda/Content/asset/js/selected.js
+++ html/nsda/nsda/Content/asset/js/selected.js
@@ -11,3 +11,7 @@ $(".time-mon li").click(function () {
     console.log($(this))
     $(this).addClass("selected").siblings().removeClass("selected")
 })
+$(".page a").click(function () {
+    console.log($(this))
+    $(this).addClass("on").siblings().removeClass("on")
+})
