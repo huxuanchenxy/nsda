2a517de013eca2f78d6711c986c51975c378d390
eigerkl@gmail.com <kuaile2>
2018-02-02 15:31:15 +0800

    click

---
+++ html/nsda/nsda/Content/asset/js/click.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    $(".common-lists li").each(function (k, v) {
+        $(v).click(function () {
+            if (!$(v).is(".selected")) {
+                $(this).addClass("selected").siblings().removeClass("selected");
+            } else {
+                $(this).removeClass("selected");
+            }
+        });
+    });
+});
--- html/nsda/nsda/Content/asset/js/selected.js
+++ html/nsda/nsda/Content/asset/js/selected.js
@@ -1,13 +1,6 @@
 ﻿
-$(".address-lists li").click(function () {
-    $(this).addClass("selected").siblings().removeClass("selected")
-})
-$(".level-lists li").click(function () {
-    $(this).addClass("selected").siblings().removeClass("selected")
-})
-$(".time-mon li").click(function () {
-    $(this).addClass("selected").siblings().removeClass("selected")
-})
+
+
 $(".page a").click(function () {
     $(this).addClass("on").siblings().removeClass("on")
 })
