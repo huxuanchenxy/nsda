674ccc41ffe46702978e65976d5b4125f93441be
eigerkl@gmail.com <kuaile2>
2018-02-05 13:49:48 +0800

    input[number]

--- html/nsda/nsda/Controllers/HomeController.cs
+++ html/nsda/nsda/Controllers/HomeController.cs
@@ -47,11 +47,6 @@ namespace WebApplication2.Controllers
             ViewBag.Message = "会员注册";
             return View();
         }
-        public ActionResult sign()
-        {
-            ViewBag.Message = "会员注册";
-            return View();
-        }
         public ActionResult playermembercenter()
         {
             ViewBag.Message = "会员中心";
